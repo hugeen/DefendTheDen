6e7c95258b263ba43a1d4387a3b53a7a452c5ebd
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-01-22 16:31:34 +0100

    create game_utils file

---
+++ js/game_utils.js
@@ -0,0 +1,17 @@
+var isMultiwayPress = function(component) {
+	return component.isDown("UP_ARROW") ||
+		component.isDown("W") ||
+		component.isDown("LEFT_ARROW") ||
+		component.isDown("A") ||
+		component.isDown("RIGHT_ARROW") ||
+		component.isDown("D") ||
+		component.isDown("DOWN_ARROW") ||
+		component.isDown("S");
+};
+
+var hitDenWalls = function(component) {
+	return component.hit("DenWallleft") ||
+		component.hit("DenWallRight") ||
+		component.hit("DenWallTop") ||
+		component.hit("DenWallBottom");
+};
