f7a9e7cadbe5ea88ae5804ad03861410366c37c4
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-01-26 14:57:59 +0100

    new audio files

---
+++ js/_audio.js
@@ -0,0 +1,31 @@
+var screenMusic = function() {
+	
+    /*var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'audio/462638_Travels_of_Vagabonds.ogg');
+    audioElement.load();
+    audioElement.addEventListener("load", function() {
+    	console.log("launched");
+    	audioElement.volume = 1;
+        audioElement.play();
+        $(".duration span").html(audioElement.duration);
+        $(".filename span").html(audioElement.src);
+    }, true);
+    $('.play').click(function() {
+        audioElement.play();
+
+    });
+    $('.pause').click(function() {
+        audioElement.pause();
+    });
+    $('.volumeMax').click(function() {
+        audioElement.volume = 1;
+    });
+    $('.volumestop').click(function() {
+        audioElement.volume = 0;
+    });
+    $('.playatTime').click(function() {
+        audioElement.currentTime = 35;
+        audioElement.play();
+    });*/
+};
+/**/
