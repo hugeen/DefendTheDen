46a8904c737bdbbe3c80fc70ab63ea3e2791bad1
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-01-29 19:35:34 +0100

    first CUT SCENE !!

--- js/_cutscenes.js
+++ js/_cutscenes.js
@@ -1,7 +1,7 @@
 var loadCutScene = function() {
 	switch(storage.level.get()) {
 		case 1: 
-			loadLevel(storage.level.get());
+			cutscenes[1]();
 			break;
 		default:
 			loadLevel(storage.level.get());
@@ -11,5 +11,58 @@ var loadCutScene = function() {
 
 var cutscenes = [];
 cutscenes[1] = function() {
+	$("body").append('<div class="wolfBig"></div><div class="pigBig"></div><div class="wolfSpeech"></div><div class="pigSpeech"></div>');
+	$(".pigSpeech").hide();
+	$(".wolfSpeech").hide();
+	$(".wolfBig").animate({
+		left: 0
+	}, 1000);
+	$(".pigBig").animate({
+		right: -10
+	}, 1000);
+	setTimeout(function() {
+		$(".pigSpeech").show();
+		$(".pigSpeech").html("Hey YOU !");
+	}, 1250);
+	setTimeout(function() {
+		$(".pigSpeech").hide();
+		$(".wolfSpeech").show();
+		$(".wolfSpeech").html(". . . ?");
+	}, 2750);
+	setTimeout(function() {
+		
+		$(".wolfSpeech").hide();
+		$(".pigSpeech").show();
+		$(".pigSpeech").html("You breath on my HOUSE !");
+	}, 4250);
+	setTimeout(function() {
+		$(".pigSpeech").hide();
+		$(".wolfSpeech").show();
+		$(".wolfSpeech").html(". . . !");
+	}, 6000);
+	setTimeout(function() {
+		$(".wolfSpeech").hide();
+		$(".pigSpeech").show();
+		$(".pigSpeech").html("Revenge ! ! !");
+	}, 8250);
 	
+	setTimeout(function() {
+		$(".wolfSpeech").hide();
+		$(".pigSpeech").hide();
+		$(".wolfBig").animate({
+			left: -300
+		}, 1000);
+		$(".pigBig").animate({
+			right: -300
+		}, 1000);
+	}, 10000);
+	
+	setTimeout(function() {
+		$(".wolfSpeech").remove();
+		$(".pigSpeech").remove();
+		$(".wolfBig").remove();
+		$(".pigBig").remove();
+		loadLevel(storage.level.get());
+	}, 11000);
+
 };
