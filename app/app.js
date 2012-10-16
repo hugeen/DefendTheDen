0a1a0a92badb9dfeaebaafb0546ba3b277c62bc5
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-10-16 08:00:11 +0200

    viewport fixed

--- app/app.js
+++ app/app.js
@@ -10,35 +10,22 @@ define([
         
         Crafty.init(config.viewport.width, config.viewport.height);
         
-        
-        // FIXME
-        $(window).resize(function() {
-            var viewport = $("#cr-stage");
-            if(viewport.height() > $("body").height()) {
-                $("#cr-stage").css({
-                    "position": "absolute",
-                    "top": "0",
-                    "left": "50%",
-                    "margin-left": "-355px",
-                    "margin-top": "0"
-                });
-                /*$("#cr-stage").removeClass("placeFixed").addClass("placeAbsolute");
-                $("#wrapper").removeClass("placeFixed").addClass("placeAbsolute");*/
-                
-            } else {
-                $("#cr-stage").css({
-                    "position": "fixed",
-                    "top": "50%",
-                    "left": "50%",
-                    "margin-top": "-290px",
-                    "margin-left": "-355px"
-                });
-                /*$("#cr-stage").removeClass("placeAbsolute").addClass("placeFixed");
-                $("#wrapper").removeClass("placeAbsolute").addClass("placeFixed");*/
+        (function initializeViewport() {
+            
+            var $viewport = $("#cr-stage, #wrapper, #inner_background");
+            function replaceViewport() {
+                if(config.viewport.height > $("body").height()) {
+                    $viewport.removeClass("placeFixed").addClass("placeAbsolute");                
+                } else {
+                    $viewport.removeClass("placeAbsolute").addClass("placeFixed");
+                }
+            };
 
-            }
-        });
-        
+            $(replaceViewport());
+            $(window).resize(function() { replaceViewport(); });
+            
+        })();
+
         Router.initialize();
         
     };
