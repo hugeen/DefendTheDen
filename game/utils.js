2cb1efcfdf8607efbe577ed8a1c4fde65d368fc9
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-10-20 10:35:07 +0200

    out of bounds util added

--- game/utils.js
+++ game/utils.js
@@ -1,6 +1,7 @@
 define([
-    'Underscore'
-], function(_) {
+    'Underscore',
+    'config'
+], function(_, config) {
 
     return {
         entityParser: function(code) {
@@ -14,6 +15,9 @@ define([
         },
         roll: function(percent) {
             return _.random(1,100) > 100 - percent;
+        },
+        outOfBounds: function(entity) {
+            return !(entity.x < config.viewport.width && entity.x > 0 && entity.y < config.viewport.height && entity.y > 0);
         }
     };
 
