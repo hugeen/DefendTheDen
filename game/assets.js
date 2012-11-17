612e483b6fed01d7e0fca873d4d3713403fcc50b
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-11-17 09:50:46 +0100

    assets file reformated

--- game/assets.js
+++ game/assets.js
@@ -1,13 +1,48 @@
 define(function() {
 
-    return [
-        { type: "images", name: "github", scenes: ["level"] },
-        { type: "sprites", name: "wolf", size: 135, scenes: ["level"] },
-        { type: "sprites", name: "pig", size: 115, scenes: ["level"] },
-        { type: "sprites", name: "octocat", size: 90, scenes: ["level"] },
-        { type: "sprites", name: "wagon", size: 81, scenes: ["level"] },
-        { type: "sprites", name: "axe", size: 42, scenes: ["level"] },
-        { type: "sprites", name: "wires", size: 42, scenes: ["level"], maps: [{ name: "wires", coords: { x: 0, y: 0, w: 1, h: 10 }}] }
-    ];
-    
+    return [{
+        type: "images",
+        name: "github",
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "wolf",
+        size: 135,
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "pig",
+        size: 115,
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "octocat",
+        size: 90,
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "wagon",
+        size: 81,
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "axe",
+        size: 42,
+        scenes: ["level"]
+    }, {
+        type: "sprites",
+        name: "wires",
+        size: 42,
+        scenes: ["level"],
+        maps: [{
+            name: "wires",
+            coords: {
+                x: 0,
+                y: 0,
+                w: 1,
+                h: 10
+            }
+        }]
+    }];
+
 });
