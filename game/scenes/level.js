d9363f33da8644264fb7c576aeef124f695d0f7d
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-11-17 20:58:10 +0100

    skill logic initialized

--- game/scenes/level.js
+++ game/scenes/level.js
@@ -5,21 +5,39 @@ define([
     "game/entities/player",
     "game/entities/monster",
     "game/entities/attack",
+    'game/keyboard',
+    'collections/skills',
     'text!templates/game_ui/skills.html',
-    'text!templates/game_ui/skill.html'
-], function($, Crafty, Burst, PlayerEntity, MonsterEntity, AttackEntity, _skills, _skill) {
+    'text!templates/game_ui/skill.html',
+    'mouseTrap'
+], function($, Crafty, Burst, PlayerEntity, MonsterEntity, AttackEntity, keyboard, skills, _skills, _skill, Mousetrap) {
 
     return {
         name: "level",
         init: function(options) {
             
             $("#wrapper").append(_.template(_skills));
-            $("#skills").append(_.template(_skill));
-            $("#skills").append(_.template(_skill));
+
+            skills.each(function(skill) {
+                var keybind = keyboard.keybinds.AZERTY[skill.get("name")]
+                
+                Mousetrap.bind(keybind, function() {
+                    console.log(skill.get("name"));
+                });
+                
+                var compiledTemplate = _.template(_skill, {
+                    key: keybind,
+                    backgroundPosition: skill.get("backgroundPosition")
+                });
+                
+                $("#skills").append(compiledTemplate);
+                
+            });
+            
             var player = PlayerEntity.create();
             
-            var monster = MonsterEntity.create("Octocat", 1);
-            var monster = MonsterEntity.create("Octocat", 3);
+            //var monster = MonsterEntity.create("Octocat", 1);
+            //var monster = MonsterEntity.create("Octocat", 3);
             Crafty.e("Wires");
             
             $("body").on("click", "#wrapper", function() {
--- game/skills.js
+++ game/skills.js
@@ -1,7 +1,29 @@
 define(function() {
-    
+
     return [{
-        name: "Attack"
+        name: "Shield",
+        backgroundPosition: "0 0"
+    }, {
+        name: "Attack",
+        backgroundPosition: "-53px 0"
+    }, {
+        name: "Blow",
+        backgroundPosition: "-106px 0"
+    }, {
+        name: "Stone",
+        backgroundPosition: "-159px 0"
+    }, {
+        name: "Root",
+        backgroundPosition: "-212px 0"
+    }, {
+        name: "Frost",
+        backgroundPosition: "-265px 0"
+    }, {
+        name: "Hast",
+        backgroundPosition: "-318px 0"
+    }, {
+        name: "Life",
+        backgroundPosition: "-371px 0"
     }];
 
 });
