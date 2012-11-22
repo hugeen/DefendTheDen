7a8961f2590e44b2d4efd8c3b6ec736ec71402a4
Cyrille Bogaert <bogaert.cyrille@gmail.com>
2012-11-22 20:23:44 +0100

    Effect refactored

--- game/entities/monster.js
+++ game/entities/monster.js
@@ -4,9 +4,14 @@ define([
 ], function(_, Crafty) {
 
     return {
-        create: function(type, line) {
+        create: function(type, line, wave) {
             var monster = Crafty.e("NonPlayableCharacter, "+type);
             monster.moveToLine(line);
+            if(typeof wave !== "undefined") {
+                monster.bind("Death", function() {
+                    wave.killMonster();
+                });
+            }
             return monster;
         }
     };
